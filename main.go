c2782bc69fd3bb441ccf4596bbe832ee4c64b0dc
marcoding-78 <marcoding78@gmail.com>
2023-02-28 16:16:24 +0330

    simple html and mutex counter

--- main.go
+++ main.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Home!")
+var counter int
+var mutex = &sync.Mutex{}
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+func aminPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi")
+	fmt.Println("Endpoint Hit: aminPage")
+}
+
+func incrementCounterPage(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
+
 func handleRequests() {
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", indexPage)
+	http.HandleFunc("/amin", aminPage)
+	http.HandleFunc("/inc", incrementCounterPage)
+
 	log.Fatal(http.ListenAndServe(":12345", nil))
 }
 

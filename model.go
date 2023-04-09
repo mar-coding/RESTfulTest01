384d38d1ff6cb349082d46d963487b4c8feeed8f
marcoding-78 <marcoding78@gmail.com>
2023-04-09 20:03:06 +0330

    add all queries that we need for the db handler

--- model.go
+++ model.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
-	"errors"
+	"fmt"
 )
 
 type Movie struct {
-	Id        int     `json:"id"`
+	Id        int64   `json:"id"`
 	Name      string  `json:"name"`
 	Year      string  `json:"year"`
 	Genre     string  `json:"genre"`
@@ -18,22 +19,56 @@ type Movie struct {
 	Link      string  `json:"link"`
 }
 
-func (p *Movie) getMovie(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (m *Movie) getMovie(db *sql.DB) error {
+	q := "SELECT * FROM Movie WHERE movie_id=?"
+	res := db.QueryRow(q, m.Id).Scan(&m.Id, &m.Name, &m.Year, &m.Genre, &m.Duration, &m.Origin, &m.Director, &m.Rate, &m.Ratecount, &m.Link)
+	return res
 }
 
-func (p *Movie) updateMovie(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (m *Movie) updateMovie(db *sql.DB) error {
+	q, err := db.Prepare("UPDATE Movie SET movie_name=?, movie_year=?, movie_genre=?, movie_duration=?, movie_origin=?, movie_director=?, movie_rating=?, movie_rating_count=?, movie_link=? WHERE movie_id=?")
+	q.Exec(m.Name, m.Year, m.Genre, m.Duration, m.Origin, m.Director, m.Rate, m.Ratecount, m.Link, m.Id)
+	return err
 }
 
-func (p *Movie) deleteMovie(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (m *Movie) deleteMovie(db *sql.DB) error {
+	q, err := db.Prepare("DELETE FROM Movie WHERE movie_id=?")
+	q.Exec(m.Id)
+	return err
 }
 
-func (p *Movie) createMovie(db *sql.DB) error {
-	return errors.New("Not implemented")
+func (m *Movie) createMovie(db *sql.DB) error {
+	q := fmt.Sprintf("INSERT INTO Movie(movie_id, movie_name, movie_year, movie_genre, movie_duration, movie_origin, movie_director, movie_rating, movie_rating_count, movie_link) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	insertResult, err := db.ExecContext(context.Background(), q, m.Id, m.Name, m.Year, m.Genre, m.Duration, m.Origin, m.Director, m.Rate, m.Ratecount, m.Link)
+	if err != nil {
+		return err
+	}
+	m.Id, err = insertResult.LastInsertId()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func getMovies(db *sql.DB, start, count int) ([]Movie, error) {
-	return nil, errors.New("Not implemented")
+	q := "SELECT * FROM Movie ORDER BY movie_id ASC LIMIT $1 OFFSET $2"
+	rows, err := db.Query(q, count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	movies := []Movie{}
+
+	for rows.Next() {
+		var m Movie
+		if err := rows.Scan(&m.Id, &m.Name, &m.Year, &m.Genre, &m.Duration, &m.Origin, &m.Director, &m.Rate, &m.Ratecount, &m.Link); err != nil {
+			return nil, err
+		}
+		movies = append(movies, p)
+	}
+
+	return movies, nil
 }
